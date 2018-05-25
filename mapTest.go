2d94408c7d793aaf420abc242b5cec1adbae07f8
rDybing <roy.dybing@gmail.com>
2018-05-25 10:49:45 +0200

    Still not working entirely...

--- mapTest.go
+++ mapTest.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"crypto/sha1"
 	"encoding/json"
 	"fmt"
+	"hash"
+	"io"
 	"math/rand"
 	"time"
 )
@@ -12,36 +15,56 @@ type myStructT struct {
 	pets []string
 }
 
-var myMap map[int]myStructT
+var myMap map[string]myStructT
 
 func main() {
+
 	rand.Seed(time.Now().UTC().UnixNano())
-	//myMap := make(map[int]myStructT)
-	myMap = initMap()
+	mySlice := initSlice()
+	myMap = initMap(mySlice)
 	outBytes, _ := json.MarshalIndent(myMap, "", "	")
 	fmt.Print(string(outBytes[:]))
 }
 
-func initMap() map[int]myStructT {
+func initSlice() []myStructT {
 	var s myStructT
-	m := make(map[int]myStructT)
-	name := [3]string{"Ole", "Dole", "Doffen"}
-	pets := [6]string{"Cat", "Dog", "Ferret", "Bunny", "Hamster", "Ferret"}
+	var out []myStructT
+
+	name := [3]string{"Huey", "Luie", "Dewey"}
+	pets := [6]string{"Cat", "Dog", "Ferret", "Bunny", "Hamster", "Parrot"}
+
 	for i := range name {
 		s.name = name[i]
+		s.pets = nil
 		x := randInt(0, 3)
-		fmt.Printf("Name: %s X: %d\n", s.name, x)
-		if x > 0 {
-			s.pets = nil
-			for j := 0; j < x; j++ {
-				y := randInt(0, 5)
-				s.pets = append(s.pets, pets[y])
-				fmt.Printf("Pet %d : %s\n", j+1, s.pets[j])
-			}
+		fmt.Printf("Name  : %s X: %d\n", s.name, x)
+		for j := 0; j < x; j++ {
+			y := randInt(0, 5)
+			s.pets = append(s.pets, pets[y])
+			fmt.Printf("Pet %d : %s\n", j+1, s.pets[j])
 		}
-		m[i] = s
+		fmt.Println("---")
+		out = append(out, s)
 	}
-	return m
+	return out
+}
+
+func initMap(in []myStructT) map[string]myStructT {
+	var h hash.Hash
+	var hash string
+	var s myStructT
+
+	out := make(map[string]myStructT)
+
+	for i := range in {
+		h = sha1.New()
+		io.WriteString(h, in[i].name)
+		hash = fmt.Sprintf("%x", h.Sum(nil))
+		s = in[i]
+		out[hash] = s
+		fmt.Println(out[hash])
+	}
+	return out
 }
 
 func randInt(min int, max int) int {
